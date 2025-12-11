75f26621de1d8133852b7ed2857b6d864413e7fa
Diggelo <joachimsvends@icloud.com>
2025-12-11 18:43:50 +0100

    add function getComputersChoice
    Adds a function that randomly chooses rock, paper or scissors.
    
    Using the math.random()

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Hello, Diggelo!")
+
+//Use the math.random to get three options for roc, paper, scissor
+function getComputersChoice() {
+    let randomNum = Math.floor(Math.random() * 3);
+    let choice = ""
+    if (randomNum === 0) {
+        choice = "Rock";
+    } else if (randomNum === 1) {
+        choice = "Paper";
+    } else if (randomNum === 2) {
+        choice = "Scissors";
+    }
+    return choice
+}
+
+console.log(getComputersChoice())
