c420b253843bf062fc38acf212fd995382be6d32
Diggelo <joachimsvends@icloud.com>
2025-12-11 19:48:04 +0100

    add function for getHumansChoice
    This function adds a prompt, so the user can input Rock, Paper og Scissors and output it til console.log()

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("Hello, Diggelo!")
 
-//Use the math.random to get three options for roc, paper, scissor
+//Use the math.random to get three options for rock, paper, scissor
 function getComputersChoice() {
     let randomNum = Math.floor(Math.random() * 3);
     let choice = ""
@@ -15,3 +15,20 @@ function getComputersChoice() {
 }
 
 console.log(getComputersChoice())
+
+
+//Get the choice from the user
+function getHumanChoice() {
+    let humanChoice = prompt("What do you choose? Rock, Paper or Scissors!: ")
+    if (humanChoice === "Rock"){
+        return console.log("You choose " + humanChoice);
+    } else if (humanChoice === "Paper") {
+        return console.log("You choose " + humanChoice);
+    } else if (humanChoice === "Scissors") {
+        return console.log("You choose " + humanChoice);
+    } else {
+        return console.log("Make a valid choice you fool!")
+    }
+}
+
+console.log(getHumanChoice())
