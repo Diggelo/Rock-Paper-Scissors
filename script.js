5d1f63175b2f9586f96aba342bd2ae090bfd7dc3
Diggelo <joachimsvends@icloud.com>
2025-12-12 13:50:38 +0100

    fixed func playRounds if else condition
    Found out that i had used else condition and return, so the code wouldn´t get past the second if condition.
    
    Now it seems like the playRound works for now! Horray!

--- script.js
+++ script.js
@@ -44,39 +44,39 @@ function playRound(humanValue, computerValue) {
     
     // TIE if human and computer got same result
     if (humanValue === computerValue) {
-        return console.log("It´s a tie, try again!")
+        return console.info("It´s a tie, try again!")
     }
 
 //human got rock, can meet computer paper or scissors (rock is a tie)
     if (humanValue === "rock" && computerValue === "scissors") {
-        return console.log("You win! Rock beats scissors.")
-    } else {
-        return console.log("You loose! Paper beats rock.")
+        return console.info("You win! Rock beats scissors.")
+    } else if (humanValue === "rock" && computerValue === "paper"){
+        return console.info("You loose! Paper beats rock.")
     }
 
  //human got paper, can meet computer scissors or rock (paper is a tie)
-    if (humanValue === "paper" && computerValue === "rock") {
-        return console.log("You win! Paper beats rock.")
-    } else {
-        return console.log("You loose! Scissors beats paper.")
+    else if (humanValue === "paper" && computerValue === "rock") {
+        return console.info("You win! Paper beats rock.")
+    } else if (humanValue === "paper" && computerValue === "scissors"){
+        return console.info("You loose! Scissors beats paper.")
     }
 
 //human got scissors, can meet computer paper or rock (scissors is a tie)
-    if (humanValue === "scissors" && computerValue === "paper") {
-        return console.log("You win! Scissors beats paper.")
-    } else {
-        return console.log("You loose! Rock beats scissors.")
+    else if (humanValue === "scissors" && computerValue === "paper") {
+        return console.info("You win! Scissors beats paper.")
+    } else if (humanValue === "scissors" && computerValue === "rock") {
+        return console.info("You loose! Rock beats scissors.")
     }
 }
 
 
-let humanSelection = getHumanChoice();
-let computerSelection = getComputersChoice();
+const humanSelection = getHumanChoice();
+const computerSelection = getComputersChoice();
 
-console.log(humanSelection)
-console.log(computerSelection)
+console.log("Check humanSelection: " + humanSelection)
+console.log("Check computerSelection: " + computerSelection)
 
 playRound(humanSelection, computerSelection);
 
-console.log("HUMAN: " + humanChoice)
-console.log("COMPUTER: " + computerChoice)
+console.log("humanChoice: " + humanChoice)
+console.log("computerChoice: " + computerChoice)
