16a9fea57d191b5acf7d7383fb5a47fbf6cb96d1
Diggelo <joachimsvends@icloud.com>
2025-12-12 14:15:58 +0100

    add forgotten semicolons in lines!

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Welcome to Rock, paper, scissor!")
+console.log("Welcome to Rock, paper, scissor!");
 
 //Use the math.random to get three options for rock, paper, scissor for computer
 function getComputersChoice() {
@@ -10,8 +10,8 @@ function getComputersChoice() {
     } else if (randomNum === 2) {
         computerChoice = "scissors";
     }
-    console.log("Computer choose " + computerChoice)
-    return computerChoice
+    console.log("Computer choose " + computerChoice);
+    return computerChoice;
 }
 
 //Get the choice from the user
@@ -20,16 +20,16 @@ function getHumanChoice() {
     humanChoice = humanChoice.toLowerCase();
     if (humanChoice === "rock"){
         console.log("You choose " + humanChoice);
-        return humanChoice
+        return humanChoice;
     } else if (humanChoice === "paper") {
         console.log("You choose " + humanChoice);
-        return humanChoice
+        return humanChoice;
     } else if (humanChoice === "scissors") {
         console.log("You choose " + humanChoice);
-        return humanChoice
+        return humanChoice;
     } else {
-        console.log("Make a valid choice you fool!")
-        return humanChoice
+        console.log("Make a valid choice you fool!");
+        return humanChoice;
     }
 }
 
@@ -40,34 +40,34 @@ function playRound(humanValue, computerValue) {
     
     // TIE if human and computer got same result
     if (humanValue === computerValue) {
-        return console.warn("It´s a tie, try again!")
+        return console.warn("It´s a tie, try again!");
     }
 
 //human got rock, can meet computer paper or scissors (rock is a tie)
     if (humanValue === "rock" && computerValue === "scissors") {
-        humanScore = humanScore + 1
-        return console.warn("You win! Rock beats scissors.")
+        humanScore = humanScore + 1;
+        return console.warn("You win! Rock beats scissors.");
     } else if (humanValue === "rock" && computerValue === "paper"){
-        computerScore = computerScore + 1
-        return console.warn("You loose! Paper beats rock.")
+        computerScore = computerScore + 1;
+        return console.warn("You loose! Paper beats rock.");
     }
 
  //human got paper, can meet computer scissors or rock (paper is a tie)
     else if (humanValue === "paper" && computerValue === "rock") {
-        humanScore = humanScore + 1
-        return console.warn("You win! Paper beats rock.")
+        humanScore = humanScore + 1;
+        return console.warn("You win! Paper beats rock.");
     } else if (humanValue === "paper" && computerValue === "scissors"){
-        computerScore = computerScore + 1
-        return console.warn("You loose! Scissors beats paper.")
+        computerScore = computerScore + 1;
+        return console.warn("You loose! Scissors beats paper.");
     }
 
 //human got scissors, can meet computer paper or rock (scissors is a tie)
     else if (humanValue === "scissors" && computerValue === "paper") {
-        humanScore = humanScore + 1
-        return console.warn("You win! Scissors beats paper.")
+        humanScore = humanScore + 1;
+        return console.warn("You win! Scissors beats paper.");
     } else if (humanValue === "scissors" && computerValue === "rock") {
-        computerScore = computerScore + 1
-        return console.warn("You loose! Rock beats scissors.")
+        computerScore = computerScore + 1;
+        return console.warn("You loose! Rock beats scissors.");
     }
 }
 
